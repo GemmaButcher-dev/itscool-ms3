717519e9d29e0c0fe8dc68fc2081a1e1825dc1e4
Gemma Butcher <gemmaford1708@icloud.com>
2024-11-13 14:42:03 +0000

    add logout route

--- app.py
+++ app.py
@@ -138,6 +138,14 @@ def profile(username):
 
     return redirect(url_for("login"))
 
+
+@app.route("/logout")
+def logout():
+    #remove user fro session cookie
+    flash("You have been logged out!")
+    session.pop("user")
+    return redirect(url_for("login"))
+
     
 if __name__ == '__main__':
     app.run(debug=True)
