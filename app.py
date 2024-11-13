ad8a5a0ce09199c101df6f561075d323100103cd
Gemma Butcher <gemmaford1708@icloud.com>
2024-11-13 14:38:16 +0000

    add login route

--- app.py
+++ app.py
@@ -66,5 +66,36 @@ def signup():
         return redirect(url_for("profile", username=session["user"]))
     return render_template("signup.html")
 
+
+#login route
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        #check if username exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            #ensure hashed password matches user input
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome, {}".format(
+                    request.form.get("username")))
+                return redirect(url_for(
+                    "profile", username=session["user"]))
+            else:
+                #invalid password
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+        
+        else:
+            #username doesnt exist
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+            
+    return render_template("login.html")
+
+
 if __name__ == '__main__':
     app.run(debug=True)
