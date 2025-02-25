7b1f4195c27818fbffb80985836c3c77e79a157b
Gemma Butcher <gemmaford1708@icloud.com>
2025-02-25 17:29:04 +0000

    keep playground as example code for targeting DB

--- db.slangs.find().js
+++ db.slangs.find().js
@@ -1,1 +1,9 @@
-db.slangs.find().pretty()
+use('school_slang'); // Select your database
+
+// Add 'pending_deletion' field to all slang documents
+db.getCollection('slangs').updateMany({}, { $set: { pending_deletion: false } });
+db.getCollection('slangs').updateMany({}, { $set: { pending_approval: false } });
+
+// Verify update
+db.getCollection('slangs').find({}, { slang: 1, pending_deletion: 1 });
+db.getCollection('slangs').find({}, { slang: 1, pending_approval: 1 });
